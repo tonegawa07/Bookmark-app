29fc93ec6360b8d1e1fdfe8eb818fb457dd5e965
tonegawa07 <fukudayusuke07@gmail.com>
2021-02-02 12:58:53 +0900

    userのCRUD-APIを作成

---
+++ api/app/controllers/v1/users_controller.rb
@@ -0,0 +1,45 @@
+class V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
+  def index
+    if params[:uid]
+      @current_user = User.find_by(uid: params[:uid])
+      render json: @current_user
+    else
+      @users = User.all
+      render json: @users
+    end
+  end
+ 
+  def show
+    render json: @user
+  end
+ 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+ 
+  def edit
+    render json: @user
+  end
+ 
+  def update
+    if @user.update(user_params)
+      render json: @user
+    end
+  end
+ 
+  private
+    def user_params
+      params.require(:user).permit(:email, :uid, :name, :profile)
+    end
+ 
+    def set_user
+      @user =User.find(params[:id])
+    end
+end
