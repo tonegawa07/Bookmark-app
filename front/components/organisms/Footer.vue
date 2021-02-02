cf28e314957e82fad77a797ba17ce0970f3bd09e
tonegawa07 <fukudayusuke07@gmail.com>
2021-02-02 12:04:03 +0900

    vueでヘッダー，フッター作成

---
+++ front/components/organisms/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+ <v-footer color="blue-grey lighten-1" dark app>
+   <v-col class="text-center" cols="12">
+   MyContent!  
+   </v-col>
+ </v-footer> 
+</template>
+   
+<script>
+export default {
+  
+}
+</script>
+   
+<style>
+    
+</style>
---
+++ front/components/organisms/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar
+  color="light-blue lighten-3" 
+  dark 
+  app 
+  fixed
+ >
+    <v-toolbar-title>
+    <n-link to="/" style="color:white; text-decoration:none;">
+       <h1 class="app-title">MyContents!</h1>
+     </n-link>
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+   
+<script>
+export default {
+  
+}
+</script>
+   
+<style>
+    
+</style>
