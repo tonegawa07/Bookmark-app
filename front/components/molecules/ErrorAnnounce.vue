ed5e4fef7e0e0d04b306f3983429d2e3ef85ca37
tonegawa07 <fukudayusuke07@gmail.com>
2021-02-03 17:56:28 +0900

    userページの作成

---
+++ front/components/molecules/ErrorAnnounce.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card 
+    v-if="status"
+    class="mx-auto mt-5 pa-5" 
+    width="400px"
+    >
+    <v-card-title>
+      <h2 class="error-box">お探しのページは見つかりませんでした。</h2>
+    </v-card-title>
+    <v-card-text>
+      <v-row justify="center">
+        <v-btn
+          color="light-blue lighten-3"
+          class="white--text"
+          to="/"
+        >
+          ホームへ戻る
+        </v-btn>
+      </v-row>
+    </v-card-text>
+  </v-card> 
+</template>
+   
+<script>
+export default {
+  props: {
+    status: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+   
+<style>
+@media (max-width: 1024px) {
+ 
+}
+
+@media (max-width: 480px) {
+ .error-box {
+   font-size: 24px;
+ }
+}
+</style>
--- front/pages/users/_id.vue
+++ front/pages/users/_id.vue
@@ -1,10 +1,51 @@
 <template>
-  <p>これは{{$route.params.id}}のユーザーページです</p>
+  <v-container>
+    <ErrorAnnounce 
+      :status="notFound"
+    />
+    <div v-if="!notFound">
+      <p>ユーザー名:{{user.name}}</p>
+      <p>メールアドレス{{user.email}}</p>
+    </div>
+  </v-container>
 </template>
    
 <script>
+import axios from '@/plugins/axios'
+import ErrorAnnounce from '~/components/molecules/ErrorAnnounce.vue'
+
 export default {
+  components: {
+    ErrorAnnounce
+  },
   
+  data() {
+    return {
+      user: {},
+      notFound: false
+    }
+  },
+
+  computed: {
+    currentUser () {
+      return this.$store.state.currentUser
+    }
+  },
+
+  mounted() {
+    axios
+        .get(`/v1/users/${this.$route.params.id}`)
+        .then((res) => {
+          const user = res.data
+          this.user = user
+        })
+        .catch((error) => {
+          if (error.response.status === 404) {
+            this.notFound = true
+          }
+        })
+  }
+
 }
 </script>
    
